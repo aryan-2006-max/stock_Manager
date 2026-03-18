4b246d0e651172eadfee3da531170ba7047f6778
aryan-2006-max <aryanbhakuni2006@gmail.com>
2026-03-19 00:20:33 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from 'srv-d6tc556uk2gs738o5ijg/context/AuthContext';
 
 const links = [
   { to: '/',             label: 'Dashboard',    end: true  },
