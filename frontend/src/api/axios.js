2a984b8b710cb0a1d56cb6e972ca074c93a718fa
aryan-2006-max <aryanbhakuni2006@gmail.com>
2026-03-19 00:24:00 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://stock-manager-xa32.onrender.com/api',
 });
 
 // Attach JWT to every request automatically
